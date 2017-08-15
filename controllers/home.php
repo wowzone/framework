e51ed9f4b2841aa96ba06cc818f6404b5c99163c
Keyser Abarca <keyser.abarca@gmail.com>
2017-08-15 02:32:17 -0600

    crea fns
    crea la function para mejorar

--- controllers/home.php
+++ controllers/home.php
@@ -1,11 +1,16 @@
 <?php
-require "config.php";
-//require "helpers.php";
+    require "config.php";
+        //require "helpers.php";
 
-$confidential ="Wow this is a private sentence";
-$language = "PHP";
-$titulo = "Mejorandola";
+    $confidential ="Wow this is a private sentence";
+    $language = "PHP";
+    $titulo = "Mejorandola";
 
-view('home', compact('language','titulo'));
+    view('home', compact('language','titulo'));
+
+    function jala()
+    {
+        echo "esto es una copia de las cosas para git";
+    }
 
 
